226d38c2464b2aab2a482f3c809629dc73bc7c22
Melissa Bell <melissabell456@gmail.com>
2018-02-09 11:47:16 -0600

    alerts are printing if plants have passed the suggested plant date

--- app/controllers/PlantAlertCtrl.js
+++ app/controllers/PlantAlertCtrl.js
@@ -2,7 +2,7 @@
 
 angular.module("myGardenApp").controller("PlantAlertCtrl", function($scope, UserPlantFctry, PlantStatsFctry) {
 
-  $scope.plantAlerts = [];
+
   let currentUser = firebase.auth().currentUser.uid;
   let today = moment().format('MM/DD/YYYY');
 
@@ -28,24 +28,51 @@ angular.module("myGardenApp").controller("PlantAlertCtrl", function($scope, User
   
   // takes object of plant which has not been watered today
   const getPlantStats = (userPlant) => {
-    // console.log("should have string on it", userPlant);
     // sends request to retrieve plant stats
+    $scope.waterAlerts = [];
     PlantStatsFctry.searchByID(userPlant.id)
     .then ( (plantStats) => {
-      $scope.waterAlerts = [];
       for (let stats in plantStats) {
         let reqWater = plantStats[stats].water_interval;
         if (userPlant.userWaterInt > reqWater) {
-          console.log(plantStats[stats]);
           $scope.waterAlerts.push(plantStats[stats]);
         }
-      console.log($scope.waterAlerts);
       }
     });
   };
 
+  // gets today's stats formatted for use with moment methods 
+  let day = moment().format('DD');
+  let month = moment().format('MM');
+  let year = moment().format('YYYY');
+  let today2 = moment([+year, +month, +day]);
+  console.log(today2);
+
   // PLANTING TIME ALERTS
   // get user's to-plant plants
+  UserPlantFctry.getUserPlants(currentUser, "to-plant")
+  .then( (to_Plant) => {
+    // console.log(to_Plant, "plant these");
+    $scope.plantAlerts = [];
+    for (let plant in to_Plant) {
+      return PlantStatsFctry.searchByID(to_Plant[plant].id)
+      .then( (plantData) => {
+        for (let plant in plantData) {
+          let plantMonth = +((plantData[plant].plant_date).slice(0,2));
+          let plantDay = +((plantData[plant].plant_date).slice(3,5));
+          let plantDate = moment([+year, +plantMonth, +plantDay]);
+          let daysUntilPlant = plantDate.diff(today2, 'days');
+          console.log(daysUntilPlant);
+          // the plant date is head of today 
+          if (daysUntilPlant <= 0) {
+            console.log(plantData[plant].name, "has passed the suggested planting date");
+            $scope.plantAlerts.push(plantData[plant]);
+          }
+        }
+      });
+    }
+  });
+
   // send ID's to firebase for plant dates
   // compare today's date to plant date. if todays date > plant date, print alert
 
