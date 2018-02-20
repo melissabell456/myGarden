1056f308603910d770895ac4abe79a933f2312e3
Melissa Bell <melissabell456@gmail.com>
2018-02-20 14:36:55 -0600

    receiving weather data from weather API and parsing to determine last rain date/days since rained. using to compare for watering alerts

---
+++ app/factories/WeatherFctry.js
@@ -0,0 +1,26 @@
+"use strict";
+
+angular.module("myGardenApp").factory("WeatherFctry", function($http, $q, API_Key) {
+
+  const getHistoricalRain = (YYYYMMDD) => {
+    return $q( (resolve, reject) => {
+      $http
+      .get(`http://api.wunderground.com/api/${API_Key.weather}/history_${YYYYMMDD}/q/TN/Nashville.json`)
+      .then( ({ data: {history: {dailysummary}} }) => {
+        let rainInches = dailysummary[0].rain;
+        if (rainInches > 0) {
+          resolve(YYYYMMDD);
+        }
+        else {
+          resolve("noRain");
+        }
+      })
+      .catch( (err) => {
+        console.log(YYYYMMDD, "fail", err);
+      });
+    });
+  };
+
+  return { getHistoricalRain};
+
+});
